c92dd60ce3c03d4780b05c65a38101a1f2dc90bb
David Ruiz Lopez <davidruizlopez2501@gmail.com>
2020-02-24 15:57:17 +0100

    now with rent routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resoureses :gears
+  resoureses :gears do
+    resoureses :rent, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
