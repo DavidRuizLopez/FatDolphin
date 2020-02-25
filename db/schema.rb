4eb61f4b0e4969901eaebdaecd68fdf0d902f1c5
Alan Ruiz <gheryk90@gmail.com>
2020-02-25 11:15:47 +0100

    Type to category changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_145039) do
+ActiveRecord::Schema.define(version: 2020_02_25_101155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_02_24_145039) do
     t.string "name"
     t.integer "price"
     t.text "description"
-    t.string "type"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_gears_on_user_id"
