b91fe952882f0a46d93e4dd2175b2246bf81900a
David Ruiz Lopez <davidruizlopez2501@gmail.com>
2020-02-25 11:26:31 +0100

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Gear.destroy_all
+
+cat = ['camera', 'tripods', 'lents']
+
+david = User.create!(email: 'david@gmail.com', password: '123456')
+bea = User.create!(email: 'bea@gmail.com', password: '123456')
+rado = User.create!(email: 'rado@gmail.com', password: '123456')
+alán = User.create!(email: 'alan@gmail.com', password: '123456')
+
+User.all.each do |u|
+  3.times do
+    gear = Gear.create!(name: Faker::Beer.name, price: 101, description: Faker::Movies::StarWars.quote, category: cat.sample, user: u)
+  end
+end
