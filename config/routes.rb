fd073b069bc80a92ec8bf621ed060ef5aecbbcf1
Alan Ruiz <gheryk90@gmail.com>
2020-02-27 12:29:34 +0100

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'dashboard', to: 'pages#dashboard'
   resources :gears do
     resources :rent, only: [:new, :create]
   end
