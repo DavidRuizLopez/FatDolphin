94deabaaec5dec9fe9359dc8f08496d80c990578
Radovan Marjanovic <marjanovic.radovan@gmail.com>
2020-02-27 15:38:13 +0100

    map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_100548) do
+ActiveRecord::Schema.define(version: 2020_02_27_100850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,9 @@ ActiveRecord::Schema.define(version: 2020_02_26_100548) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_gears_on_user_id"
   end
 
